fa120437bb2c00b9e603bd6bed543c1eb00c691b
Ricky Liu <rorix12321@gmail.com>
2018-08-12 04:38:50 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1,2]
+end
+
+def first_element(array1)
+  array1[0]
+end
+
+def third_element(array1)
+  array1[2]
+end
+
+def last_element(array1)
+  array1[-1]
+end
+
+def first_element_with_array_methods(array1)
+  array1.shift
+end
+
+def last_element_with_array_methods(array1)
+  array1.pop
+end
+
+def length_of_array(array1)
+  array1.length
+end
